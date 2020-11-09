02950ac23329da90db762c0fc75cb0987df37d0d
Alan Dolan <alco139@gmail.com>
2020-11-09 15:26:54 +0100

    login,register,join, home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,18 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'join',
+    loadChildren: () => import('./pages/join/join.module').then( m => m.JoinPageModule)
+  },
 ];
 
 @NgModule({
